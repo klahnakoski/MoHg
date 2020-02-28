2dd0bc4ee93e08bd4c0adf486f10e30b4595ea6b
Kyle Lahnakoski <kyle@lahnakoski.com>
2020-02-28 18:31:19 -0500

    better test

--- tests/test.py
+++ tests/test.py
@@ -10,6 +10,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
 
+from mo_json import value2json
+
 from jx_elasticsearch.elasticsearch import Cluster
 from mo_dots import Null, wrap, coalesce
 from mo_files import File
@@ -42,26 +44,50 @@ class TestHg(FuzzyTestCase):
         self.hg = HgMozillaOrg(TestHg.config)
 
     def test_get_push1(self):
-        central = [b for b in self.hg.branches if b.name == "mozilla-central" and b.locale == "en-US"][0]
+        central = [
+            b
+            for b in self.hg.branches
+            if b.name == "mozilla-central" and b.locale == "en-US"
+        ][0]
         test = self.hg._get_push(central, "b6b8e616de32")
-        expected = {"date": 1503659542, "user": "archaeopteryx@coole-files.de", "id": 32390}
+        expected = {
+            "date": 1503659542,
+            "user": "archaeopteryx@coole-files.de",
+            "id": 32390,
+        }
         self.assertEqual(test, expected)
         while len(self.hg.todo.queue):
             Till(seconds=1).wait()
 
     def test_get_rev_with_backout(self):
-        central = [b for b in self.hg.branches if b.name == "mozilla-central" and b.locale == "en-US"][0]
-        test = self.hg.get_revision(wrap({"branch":central, "changeset":{"id":"de7aa6b08234"}}))
-        expected = {"changeset": {"backedoutby": "f384789a29dcfd514d25d4a16a97ec5309612d78"}}
+        central = [
+            b
+            for b in self.hg.branches
+            if b.name == "mozilla-central" and b.locale == "en-US"
+        ][0]
+        test = self.hg.get_revision(
+            wrap({"branch": central, "changeset": {"id": "de7aa6b08234"}})
+        )
+        expected = {
+            "changeset": {"backedoutby": "f384789a29dcfd514d25d4a16a97ec5309612d78"}
+        }
         self.assertEqual(test, expected)
         while len(self.hg.todo.queue):
             Till(seconds=1).wait()
 
     def test_get_prefix_space(self):
-        central = [b for b in self.hg.branches if b.name == "mozilla-central" and b.locale == "en-US"][0]
-        test = self.hg.get_revision(wrap({"branch": central, "changeset": {"id": "de7aa6b08234"}}), None, True)
+        central = [
+            b
+            for b in self.hg.branches
+            if b.name == "mozilla-central" and b.locale == "en-US"
+        ][0]
+        test = self.hg.get_revision(
+            wrap({"branch": central, "changeset": {"id": "de7aa6b08234"}}), None, True
+        )
 
-        self.assertTrue(test.changeset.diff[1].changes[0].new.content.startswith("    "))
+        self.assertTrue(
+            test.changeset.diff[1].changes[0].new.content.startswith("    ")
+        )
 
     def test_diff_to_json(self):
         j1 = diff_to_json(File("tests/resources/diff1.patch").read())
@@ -75,49 +101,112 @@ class TestHg(FuzzyTestCase):
     def test_big_changeset_to_json(self):
         big_patch_file = File("tests/resources/big.patch")
         # big_patch_file.write_bytes(http.get("https://hg.mozilla.org/mozilla-central/raw-rev/e5693cea1ec944ca077c7a46c5f127c828a90f1b").content)
-        self.assertEqual(b'\r'.decode('utf8', 'replace'), u'\r')
+        self.assertEqual(b"\r".decode("utf8", "replace"), "\r")
 
         j1 = diff_to_json(big_patch_file.read_bytes().decode("utf8", "replace"))
-        expected = File("tests/resources/big.json").read_json(flexible=False, leaves=False)
+        expected = File("tests/resources/big.json").read_json(
+            flexible=False, leaves=False
+        )
         self.assertEqual(j1, expected)
 
     def test_small_changeset_to_json(self):
         small_patch_file = File("tests/resources/small.patch")
 
         j1 = diff_to_json(small_patch_file.read_bytes().decode("utf8", "replace"))
-        expected = File("tests/resources/small.json").read_json(flexible=False, leaves=False)
+        expected = File("tests/resources/small.json").read_json(
+            flexible=False, leaves=False
+        )
         self.assertEqual(j1, expected)
 
     def test_changeset_to_json(self):
         hg_mozilla_org.IGNORE_MERGE_DIFFS = False
         j1 = self.hg.get_revision(
-            wrap({
-                "branch": {"name": "mozilla-central", "url": "https://hg.mozilla.org/mozilla-central"},
-                "changeset": {"id": "e5693cea1ec944ca0"}
-            }),
+            wrap(
+                {
+                    "branch": {
+                        "name": "mozilla-central",
+                        "url": "https://hg.mozilla.org/mozilla-central",
+                    },
+                    "changeset": {"id": "e5693cea1ec944ca0"},
+                }
+            ),
             None,  # Locale
-            True   # get_diff
+            True,  # get_diff
         )
         hg_mozilla_org.IGNORE_MERGE_DIFFS = True
 
-        expected = File("tests/resources/big.json").read_json(flexible=False, leaves=False)
+        expected = File("tests/resources/big.json").read_json(
+            flexible=False, leaves=False
+        )
         self.assertEqual(j1.changeset.diff, expected)
 
     def test_coverage_parser(self):
-        diff = http.get('https://hg.mozilla.org/mozilla-central/raw-rev/14dc6342ec5').content.decode('utf8')
+        diff = http.get(
+            "https://hg.mozilla.org/mozilla-central/raw-rev/14dc6342ec5"
+        ).content.decode("utf8")
         moves = diff_to_moves(diff)
-        Log.note("{{files}}", files=[m.old.name if m.new.name=='dev/null' else m.new.name for m in moves])
-
+        Log.note(
+            "{{files}}",
+            files=[
+                m.old.name if m.new.name == "dev/null" else m.new.name for m in moves
+            ],
+        )
 
     def test_revision_with_bug(self):
         rev = self.hg.get_revision(
-            wrap({
-                "branch": {"name": "mozilla-central", "url": "https://hg.mozilla.org/mozilla-central"},
-                "changeset": {"id": 'fc9d28ae4655'}
-            }),
+            wrap(
+                {
+                    "branch": {
+                        "name": "mozilla-central",
+                        "url": "https://hg.mozilla.org/mozilla-central",
+                    },
+                    "changeset": {"id": "fc9d28ae4655"},
+                }
+            ),
             None,
-            True
+            False,
+            False,
         )
-        expected = {}
+        expected = {
+            "branch": {
+                "description": 'Current Firefox and Gecko development. Releases branch off of this repository. Also known as "Trunk"',
+                "etl": {"timestamp": 1582913804.2270637},
+                "last_used": 1582906787,
+                "locale": "en-US",
+                "name": "mozilla-central",
+                "parent_name": "Mozilla top level repositories",
+                "url": "https://hg.mozilla.org/mozilla-central/",
+            },
+            "changeset": {
+                "author": "Tom Schuster <evilpies@gmail.com>",
+                "bug": 1549641,
+                "date": 1582891980,
+                "description": "Bug 1549641 - Add devtools test for overwritten name and message in error object r=bzbarsky\n\nI had problems running this test locally and it turns out cssErrorReportingEnabled is not\nset/unset correctly. Thanks to Boris for pointing that out.\n\nDifferential Revision: https://phabricator.services.mozilla.com/D64415",
+                "files": [
+                    "devtools/shared/webconsole/test/chrome/chrome.ini",
+                    "devtools/shared/webconsole/test/chrome/test_cached_messages.html",
+                    "devtools/shared/webconsole/test/chrome/test_page_errors.html",
+                    "devtools/shared/webconsole/test/chrome/chrome.ini",
+                    "devtools/shared/webconsole/test/chrome/test_cached_messages.html",
+                    "devtools/shared/webconsole/test/chrome/test_page_errors.html",
+                ],
+                "id": "fc9d28ae4655254a6ef8a57ac6577545b2a42ca7",
+                "id12": "fc9d28ae4655",
+            },
+            "etl": {
+                "machine": {
+                    "name": "klahnakoski-39477",
+                    "os": "Windows10",
+                    "pid": 9640,
+                    "python": "CPython",
+                },
+                "timestamp": 1582922391.0164626,
+            },
+            "index": 516105,
+            "landingsystem": "lando",
+            "parents": "4bdb67b0a4f58d41158520892a459894f9990bd4",
+            "phase": "public",
+            "push": {"date": 1582906780, "id": 37166, "user": "malexandru@mozilla.com"},
+        }
 
         self.assertEqual(rev, expected)
